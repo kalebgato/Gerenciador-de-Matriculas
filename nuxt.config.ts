904b839314f3484a39db58681080fa774d7f566d
Káleb Gato <ktgato@stefanini.com>
2026-04-13 13:52:37 -0400

    feat: integrar Tailwind CSS ao projeto e atualizar configurações do Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-export default defineNuxtConfig({})
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2026-04-13',
@@ -10,11 +11,19 @@ export default defineNuxtConfig({
     }
   },
 
+  css: ['./app/assets/css/main.css'],
+
   modules: [
     '@nuxt/a11y',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxtjs/color-mode',
-  ]
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
